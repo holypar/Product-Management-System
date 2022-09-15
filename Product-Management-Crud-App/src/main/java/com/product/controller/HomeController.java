432f26c679a84feddba51fad4000bce98fe66386
holypar <singhparminder9411@gmail.com>
2022-09-14 19:58:58 -0700

    CRUD complete!

--- Product-Management-Crud-App/src/main/java/com/product/controller/HomeController.java
+++ Product-Management-Crud-App/src/main/java/com/product/controller/HomeController.java
@@ -1,11 +1,16 @@
 package com.product.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.product.entity.Products;
@@ -20,7 +25,10 @@ public class HomeController {
 	private ProductRepository productRepo;
 
 	@GetMapping("/") // the route
-	public String home() {
+	public String home(Model m) {
+		List<Products> list = productRepo.findAll();
+		m.addAttribute("all_products", list); // when we call the list in html call it by all_products
+
 		return "index"; // the html file name in template dir for the route
 	}
 
@@ -29,8 +37,11 @@ public class HomeController {
 		return "add";
 	}
 
-	@GetMapping("/edit_form")
-	public String editForm() {
+	@GetMapping("/edit_form/{id}")
+	public String editForm(@PathVariable(value = "id") long id, Model m) {
+		Optional<Products> product = productRepo.findById(id);
+		Products pro = product.get();
+		m.addAttribute("product", pro);
 		return "edit";
 	}
 
@@ -40,6 +51,23 @@ public class HomeController {
 		productRepo.save(products);
 		session.setAttribute("msg", "Product Added Successfuly");
 		return "redirect:/load_form";
+	}
+
+	@PostMapping("/update_products")
+	public String updateProducts(@ModelAttribute Products products, HttpSession session) {
+
+		productRepo.save(products);
+		session.setAttribute("msg", "Product Updated Successfuly");
+		return "redirect:/";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteProducts(@PathVariable(value = "id") long id, HttpSession session) {
+		productRepo.deleteById(id);
+		session.setAttribute("msg", "Product Deleted Successfuly");
+
+		return "redirect:/";
+
 	}
 
 }
